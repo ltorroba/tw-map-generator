925d3eac28b46f47d69f96e8773754042605d5f2
Lucas Torroba <lucasfth@gmail.com>
2015-08-18 10:08:32 -0300

    Initial Commit

---
+++ TWMapGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TWMapGenerator
+//
+//  Created by Lucas Torroba on 8/18/15.
+//  Copyright (c) 2015 Lucas Torroba. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
